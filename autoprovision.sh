1e3c7445cba275f3bfad0db38a208e79ef54215c
Justin Ladwig <justin.ladwig.2003@gmail.com>
2025-05-19 17:06:27 +0200

    Add prompts before each build and deployment step in autoprovision script

--- autoprovision.sh
+++ autoprovision.sh
@@ -7,16 +7,26 @@ PACKER_UBUNTU_DIR="./packer-ITM/ubuntu-server-noble"
 PACKER_OPNSENSE_DIR="./packer-ITM/opnsense"
 TERRAFORM_DIR="./terraform-ITM"
 
+printf "%s " "Press enter to start Packer Ubuntu Build"
+read ans
+
 echo "*** Starte Packer Ubuntu Build ***"
 cd "$PACKER_UBUNTU_DIR"
 packer init .
 packer build -var "proxmox_vm_id=1000" -var "proxmox_node=$PROXMOX_NODE" .
 cd ~/ITM-Projekt
 
+
+printf "%s " "Press enter to start Packer OPNsense Build"
+read ans
+
 echo "*** Starte Packer OPNsense Build ***"
 cd "$PACKER_OPNSENSE_DIR"
 packer build -var "proxmox_vm_id=1001" -var "proxmox_node=$PROXMOX_NODE" .
 cd ~/ITM-Projekt
+
+printf "%s " "Press enter to start Terraform Deployment"
+read ans
 
 echo "*** Starte Terraform Deployment ***"
 cd "$TERRAFORM_DIR"
